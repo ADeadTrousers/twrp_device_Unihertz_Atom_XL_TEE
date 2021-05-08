093045e324f8ebb8c2d7fa816f7e2fc8f7d8517f
Matthias Leitl <a.dead.trousers@gmail.com>
2021-05-08 20:23:20 +0200

    Remove vendor blobs from source tree

--- Android.mk
+++ Android.mk
@@ -1,7 +1,6 @@
+LOCAL_PATH := $(call my-dir)
+
 ifneq ($(filter Atom_XL_TEE ,$(TARGET_DEVICE)),)
-
-LOCAL_PATH := device/Unihertz/Atom_XL_TEE
-
-include $(call all-makefiles-under,$(LOCAL_PATH))
-
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 endif
