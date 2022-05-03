a05891a92b030a2d31f0ceacdbe58bdbcf72af4c
Matthias Leitl <a.dead.trousers@gmail.com>
2022-05-03 19:28:39 +0200

    Update fingerprint

--- twrp_Atom_XL_TEE.mk
+++ twrp_Atom_XL_TEE.mk
@@ -31,8 +31,8 @@ PRODUCT_MANUFACTURER := Unihertz
 PRODUCT_BOARD := g63v71c2k_dfl_stee
 
 # Add fingerprint from Stock ROM build.prop
-BUILD_FINGERPRINT := "Unihertz/Atom_XL/Atom_XL:10/QP1A.190711.020/root.20200928.094206:user/release-keys"
+BUILD_FINGERPRINT := "Unihertz/Atom_XL/Atom_XL:11/RP1A.200720.011/root.20210729.112847:user/release-keys"
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=Atom_XL_TEE \
     PRODUCT_NAME=Atom_XL \
-    PRIVATE_BUILD_DESC="Atom_XL-user 10 QP1A.190711.020 root.20200928.094206 release-keys"
+    PRIVATE_BUILD_DESC="Atom_XL-user 11 RP1A.200720.011 root.20210729.112847 release-keys"
